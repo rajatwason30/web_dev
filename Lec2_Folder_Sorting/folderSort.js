098a548500d7a7bcc890a89d71ada08baa863889
Rajat <rajatwason30@gmail.com>
2021-05-26 00:15:30 +0530

    Recursive Folder Sorting

---
+++ Lec2_Folder_Sorting/folderSort.js
@@ -0,0 +1,51 @@
+const fs=require("fs");
+const path=require("path");
+let extension=require("./utils.js")
+let folderPath = "./Downloads";
+
+function getFolderPath(extensionName,folderPath)
+{
+    //checks of a folder exists for given extension
+    // if exists returns a path to that folder
+    // else creates a new folder and returns a path to that folder
+    let fileFolderPath=folderPath;
+    for(let key in extension)
+    {
+        if(extension[key].includes(extensionName))
+        {
+            fileFolderPath+="/"+key;
+            break;
+        }
+    }
+    if(!fs.existsSync(fileFolderPath))
+    {
+        //create new folder and then return this
+        fs.mkdirSync(fileFolderPath);
+    }
+    return fileFolderPath;
+}
+
+function sortFolder(folderPath)
+{
+    fileList=fs.readdirSync(folderPath);
+    for(let i=0;i<fileList.length;i++)
+    {
+        let filename=fileList[i];
+        let extensionName=path.extname(filename);
+        if(extensionName.length==0)
+        {
+            //recursively sort other folders within downloads folder 
+            //a new folder. Sort this folder as well
+            let nextFolderPath=folderPath+"/"+filename;
+            sortFolder(nextFolderPath);
+        }
+        let fileFolderPath=getFolderPath(extensionName,folderPath);
+        //move files
+        let srcpath=folderPath+"/"+filename; //"./Downloads/abc.jpg"
+        let dstpath=fileFolderPath+"/"+filename;
+        fs.copyFileSync(srcpath,dstpath);
+        fs.unlinkSync(srcpath);
+    }
+}
+
+sortFolder(folderPath);
---
+++ Lec2_Folder_Sorting/utils.js
@@ -0,0 +1,9 @@
+extension={
+    "Audio": [".mp3"],
+    "Images": [".jpg","jpeg",".gif",".png"],
+    "Videos": [".mp4",".mkv"],
+    "Compressed": [".zip"],
+    "Documents": [".doc",".xlsx"]
+};
+
+module.exports=extension;
